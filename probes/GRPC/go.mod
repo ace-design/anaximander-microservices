127eb593c8ec534166855838beaa3b91432b7774
Jean-Philippe Caissy <jpcaissy@piji.ca>
2020-06-10 11:40:32 -0400

    Refactored into packages to be reused

--- probes/GRPC/go.mod
+++ probes/GRPC/go.mod
@@ -3,9 +3,19 @@ module github.com/ace-design/anaximander-microservices/probes/GRPC
 go 1.14
 
 require (
+	contrib.go.opencensus.io/exporter/jaeger v0.2.0 // indirect
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.1 // indirect
+	github.com/GoogleCloudPlatform/microservices-demo v0.2.0 // indirect
 	github.com/ace-design/anaximander-microservices/utils/go/parser v0.0.0-00010101000000-000000000000
+	github.com/google/pprof v0.0.0-20200604032702-163a225fb653 // indirect
+	github.com/googleapis/gax-go v1.0.3 // indirect
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/prometheus/client_golang v1.6.0 // indirect
 	github.com/sirupsen/logrus v1.6.0
+	github.com/streadway/handy v0.0.0-20200128134331-0f66f006fb2e // indirect
+	github.com/weaveworks/common v0.0.0-20200512154658-384f10054ec5 // indirect
 	golang.org/x/tools v0.0.0-20200609124132-5359b67ffbdf
 )
 
 replace github.com/ace-design/anaximander-microservices/utils/go/parser => /home/jpcaissy/src/anaximander-microservices/util/go/parser
+replace github.com/ace-design/anaximander-microservices/probes/GRPC => /home/jpcaissy/src/anaximander-microservices/probes/GRPC
--- util/go/parser/parser.go
+++ util/go/parser/parser.go
@@ -10,6 +10,10 @@ import (
 	"go/token"
 	"go/types"
 
+	go_parser "go/parser"
+	"golang.org/x/tools/go/packages"
+	"golang.org/x/tools/go/ssa/ssautil"
+
 	"golang.org/x/tools/go/ssa"
 
 	log "github.com/sirupsen/logrus"
@@ -30,13 +34,15 @@ type Visitor struct {
 	VisitedBlocks map[*ssa.BasicBlock]bool
 	Root          *Node
 
+	Probe interface{}
+
 	VisitCall func(*Visitor, *ssa.Call)
 }
 
-func NewVisitor(p *ast.Package) *Visitor {
+func NewVisitor(visitCall func(*Visitor, *ssa.Call)) *Visitor {
 	return &Visitor{
 		Imports:       make(map[string]*ast.ImportSpec),
-		Package:       p,
+		VisitCall: visitCall,
 		VisitedFuncs:  make(map[*ssa.Function]bool),
 		VisitedBlocks: make(map[*ssa.BasicBlock]bool),
 	}
@@ -165,4 +171,56 @@ func (v *Visitor) ParseImports() error {
 	}
 
 	return nil
+}
+
+func Parse(path string, visitor *Visitor) {
+	fset := token.NewFileSet() // positions are relative to fset
+	prog, err := go_parser.ParseDir(fset, path, nil, 0)
+	if err != nil {
+		log.Error(err)
+	}
+
+	for name, pkg := range prog {
+		visitor.Package = pkg
+		log.WithFields(log.Fields{
+			"package": name,
+			"imports": pkg.Imports,
+		}).Info("Visiting package")
+
+		err := visitor.ParseImports()
+		if err != nil {
+			log.WithError(err).Debug("Failed to parse imports")
+		}
+
+		files := []string{}
+		for filename, _ := range pkg.Files {
+			files = append(files, filename)
+		}
+
+		cfg := &packages.Config{Mode: packages.LoadSyntax}
+		initial, err := packages.Load(cfg, files...)
+		if err != nil {
+			log.WithError(err).Fatal("Failed to laod package")
+		}
+
+		prog, pkgs := ssautil.AllPackages(initial, ssa.SanityCheckFunctions|ssa.BareInits|ssa.GlobalDebug)
+		for i, p := range pkgs {
+			if p == nil {
+				log.WithFields(log.Fields{
+					"package": initial[i],
+				}).Fatal("cannot build SSA")
+			}
+		}
+		prog.Build()
+		for _, main := range ssautil.MainPackages(pkgs) {
+			entrypoint := main.Func("main")
+			visitor.Root = &Node{
+				Func:     entrypoint,
+				Children: []*Node{},
+			}
+			visitor.VisitedFuncs[entrypoint] = true
+			visitor.Visit(entrypoint.Blocks[0], visitor.Root)
+		}
+	}
+
 }
