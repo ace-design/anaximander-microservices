a84e82cbb2405e3c8a3b6af1600a205fbdffd42d
Spechen <florian.vouters@viacesi.fr>
2020-06-16 17:56:09 +0200

    Change the way that the probes find all files in a given directory

--- util/findFiles.js
+++ util/findFiles.js
@@ -27,6 +27,34 @@ exports.walkFiles = async function(dir, done) {
     return results;
 }
 
-exports.isFile = function(path) {
-    return path.isFile(path);
+exports.walk = function(dir) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dir, (error, files) => {
+            if (error) {
+                return reject(error);
+            }
+            Promise.all(files.map((file) => {
+                return new Promise((resolve, reject) => {
+                    const filepath = path.join(dir, file);
+                    fs.stat(filepath, (error, stats) => {
+                        if (error) {
+                            return reject(error);
+                        }
+                        if (stats.isDirectory()) {
+                            this.walk(filepath).then(resolve);
+                        } else if (stats.isFile() && path.extname(filepath) === ".js") {
+                            resolve(filepath);
+                        }
+                    });
+                });
+            }))
+                .then((foldersContents) => {
+                    resolve(foldersContents.reduce((all, folderContents) => all.concat(folderContents), []));
+                });
+        });
+    });
+}
+
+exports.isFile = function(pathf) {
+    return fs.lstatSync(pathf).isFile();
 }
