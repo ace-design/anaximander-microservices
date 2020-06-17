d5433a855615b4b4bc1b6eb7e9ca59aab8618336
Spechen <florian.vouters@viacesi.fr>
2020-06-17 11:23:49 +0200

    Add docker and update UsageByRoute probe
    This probes is the last to be dockerize.
    
    And update the format of the output of this probe to match with others, will now output all routes of services and for each route, it will output redis usage or mongocollections usage and all usage on each collection (findOne, instertOne, etc)

--- probes/UsageByRoute/findUsageByRoute.js
+++ probes/UsageByRoute/findUsageByRoute.js
@@ -1,54 +1,274 @@
 const walkers = require('../../util/walkers');
+const aParser = require('../../util/acornParser');
+const fFiles = require('../../util/findFiles');
+const fs = require('fs');
 
-class findUsageByRoute {
+(async function() {
 
-    constructor(body, route, redis, redisClient, mongoDB, mongoCollections) {
-        this.body = body;
-        this.route = route;
-        this.redis = redis;
-        this.redisClient = redisClient;
-        this.mongoDB = mongoDB;
-        this.mongoCollections = mongoCollections;
-        this.result = {};
+    let file = process.argv[2];
+    let filesContent = [];
+    if (fFiles.isFile(file)) {
+        filesContent.push({
+            file : file,
+            body : aParser.acornParser(file),
+        });
     }
-
-    run() {
-        if (!this.body) return null;
-        let routeNode = walkers.recursiveWalkIn(this.body, "Literal", this.route.route)[1][0][0];
-        routeNode = walkers.getAncestors(this.body, routeNode)[2][2];
-        if (this.redisClient) {
-            routeNode.arguments.forEach(a => {
-                if (a.body && a.body.body) {
-                    a.body.body.forEach(b => {
-                        if (b.expression
-                            && b.expression.callee.object.name === this.redisClient) {
-                            this.result.redis = true;
-                        }
-                    });
-                }
+    else if (fs.lstatSync(file).isDirectory()) {
+        let results = await fFiles.walk(file);
+        results.forEach( subFile => {
+            filesContent.push({
+                file : subFile,
+                body : aParser.acornParser(subFile),
             });
-        }
-        if (this.mongoDB && this.mongoCollections) {
-            this.result.mongoDB = true;
-            if (typeof this.result.mongoCollections === "undefined") this.result.mongoCollections = [];
-            this.mongoCollections.forEach(mc => {
-                let uses = walkers.recursiveWalkInArray(routeNode.arguments[1], "Identifier", mc);
-                let actionOnCollection = [];
-                uses.forEach(u => {
-                    let action = u.property.name;
-                    if (!actionOnCollection.includes(action)) {
-                        actionOnCollection.push(action);
+        });
+    }
+    let returnString = [];
+    filesContent.forEach(fC => {
+        let vertices = []; let i = 0;
+        let redisClient = getRedisClient(fC.body, getRedis(fC.body));
+        let mongoCollections = getMongoCollection(fC.body);
+        let routes = getRoutes(fC.body, findAppString(fC.body));
+        routes.forEach(route => {
+            route.id = "r" + i;
+            i++;
+            let resultString = getUsageByRoutes(fC.body, route, redisClient, mongoCollections.length > 0, mongoCollections);
+            vertices.push(formatVertice(fC, route, resultString));
+        });
+        returnString.push(display(fC, vertices));
+    });
+    console.log(JSON.stringify(returnString));
+    return returnString;
+}())
+
+function getUsageByRoutes(body, route, redisClient, mongoDB, mongoCollections) {
+    let result = [];
+    if (!body) return null;
+    let routeNode = walkers.recursiveWalkIn(body, "Literal", route.route)[1][0][0];
+    routeNode = walkers.getAncestors(body, routeNode)[2][2];
+    if (redisClient) {
+        routeNode.arguments.forEach(a => {
+            if (a.body && a.body.body) {
+                a.body.body.forEach(b => {
+                    if (b.expression
+                        && b.expression.callee.object.name === redisClient) {
+                        result.redis = true;
                     }
                 });
-                if (uses.length > 0) {
-                    this.result.mongoCollections.push({
-                        [mc] : actionOnCollection
-                    });
-                    console.log(this.result.mongoCollections);
+            }
+        });
+    }
+    if (mongoDB && mongoCollections) {
+        result.mongoDB = true;
+        if (typeof result.mongoCollections === "undefined") result.mongoCollections = [];
+        mongoCollections.forEach(mc => {
+            let uses = walkers.recursiveWalkInArray(routeNode.arguments[1], "Identifier", mc.varName);
+            let actionOnCollection = [];
+            uses.forEach(u => {
+                let action = u.property.name;
+                if (!actionOnCollection.includes(action)) {
+                    actionOnCollection.push(action);
                 }
             });
+            if (uses.length > 0) {
+                result.mongoCollections.push({
+                    [mc.collectionName] : actionOnCollection
+                });
+            }
+        });
+    }
+    return result;
+}
+
+function getRoutes(body, appString) {
+    let routes = [];
+    let appUsage = walkers.recursiveWalkInArray(body, "Identifier", appString);
+    appUsage.forEach(au => {
+        if (au.type === "MemberExpression") {
+            let auAncestors = walkers.getAncestors(body, au);
+            auAncestors = auAncestors[2];
+            let auAncestor = auAncestors[1];
+            if (auAncestor.arguments && auAncestor.arguments.length > 0) {
+                auAncestor.arguments.forEach(a => {
+                    if (a.type === "Literal" &&
+                        a.value !== 'env' &&
+                        au.property.name !== 'use' &&
+                        au.property.name !== "listen") {
+                        routes.push({
+                            type : au.property.name,
+                            route : a.value
+                        });
+                    }
+                })
+            }
         }
+    });
+    return routes;
+}
+
+function findAppString(body) {
+    let expressString = walkers.recursiveWalkIn(body, "Literal", "express");
+    expressString = expressString[1][0][0].value;
+    let appString = walkers.recursiveWalkInArray(body, "Identifier", expressString);
+    appString = appString[appString.length -1];
+    ancestors = walkers.getAncestors(body, appString);
+    ancestors = ancestors[2];
+    ancestors.forEach(an => {
+        if (an.type === "VariableDeclarator" &&
+            an.init &&
+            an.init.callee &&
+            an.init.callee.name === expressString) {
+            appString = an.id.name;
+        }
+    });
+    return appString;
+}
+
+function getMongoCollection(body) {
+    let collections = [];
+    let [success, mongoConnect, ancestors] = walkers.recursiveWalkIn(body, "Identifier", "connect");
+    if (!success) return [];
+    let ancestorsDbClient = ancestors[2];
+    let dbClient, db;
+    if (ancestorsDbClient.arguments[1].params[1].name) {
+        dbClient = ancestorsDbClient.arguments[1].params[1].name;
+        if (ancestorsDbClient.arguments[1].body.body[0].alternate) {
+            let [success, dbClientNode, dbClientAncestors] = walkers.recursiveWalkIn(ancestorsDbClient.arguments[1].body, "Identifier", dbClient);
+            if (success) {
+                db = dbClientAncestors[3].left.name;
+            }
+        }
+
+    }
+    if (db) {
+        let nodes = walkers.recursiveWalkInArray(ancestorsDbClient, "Identifier", db);
+        nodes.forEach(n => {
+            if (n.object &&
+                n.object.name === db &&
+                n.property &&
+                n.property.name.toLocaleLowerCase() === "collection") {
+                let nodeAncestors = walkers.getAncestors(body, n);
+                nodeAncestors = nodeAncestors[2];
+                collections.push({
+                    varName : nodeAncestors[2].left.name,
+                    collectionName : nodeAncestors[2].right.arguments[0].value
+                });
+            }
+        })
+    }
+    return collections;
+}
+
+function getRedisClient(body, redisVar) {
+    let redisClient = walkers.recursiveWalkInArray(body, "Identifier", redisVar);
+    let resultString;
+    redisClient.forEach(rc => {
+        if (resultString) return;
+        let rcAncestors = walkers.getAncestors(body, rc);
+        if (rcAncestors[0]) {
+            rcAncestors = rcAncestors[2];
+            rcAncestors.forEach(rcA => {
+                if (rcA.type === "VariableDeclarator"
+                    && rcA.id
+                    && rcA.init
+                    && rcA.init.callee
+                    && rcA.init.callee.object
+                    && rcA.init.callee.object.name === redisVar) {
+                    resultString = rcA.id.name;
+                }
+            })
+        }
+    });
+    return resultString;
+}
+
+function getRedis(body) {
+    let redisString = walkers.recursiveWalkIn(body, "Literal", "redis");
+    redisString = redisString[1];
+    if (redisString[0] && redisString[0][0] && redisString[0][0].value) {
+        return redisString[0][0].value;
+    } else {
+        return null;
     }
 }
 
-module.exports = findUsageByRoute;
+function formatVertice(fContent, route, result) {
+    if (route) {
+        let service = fContent.file.split('/').filter(s => {
+            return s.toLocaleLowerCase().indexOf('service') > -1;
+        });
+        let graphJSON = {
+            id : route.id,
+            type : "route",
+            props : [
+                {
+                    name: "path",
+                    value: route.route,
+                },
+                {
+                    name : "verb",
+                    value: route.type,
+                }
+            ]
+        };
+        if (result.redis) {
+            graphJSON.props.push({
+                name : "redis",
+                value : true
+            })
+        }
+        if (result.mongoCollections.length > 0) {
+            result.mongoCollections.forEach( mc => {
+                graphJSON = objectHasMongoCollections(graphJSON);
+                let index = getMongoCollectionsIndex(graphJSON);
+                let keys = Object.keys(mc);
+                //console.log(mc[keys[0]]);
+                for (let key of keys) {
+                    mc[key].forEach(mcu => {
+                        graphJSON.props[index].value.push({
+                            name : key,
+                            value : mcu
+                        })
+                    })
+                }
+            })
+        }
+        console.log(graphJSON.props);
+        return graphJSON;
+    }
+}
+
+function objectHasMongoCollections(graphJSON) {
+    let hasMC = false;
+    graphJSON.props.forEach(prop => {
+        if (prop.name.toLocaleLowerCase() === "mongocollections") {
+            hasMC = true;
+        }
+    });
+    if (!hasMC) {
+        graphJSON.props.push({
+            name : "MongoCollections",
+            value : []
+        });
+    }
+    return graphJSON;
+}
+
+function getMongoCollectionsIndex(graphJSON) {
+    for (let i =0; i<graphJSON.props.length; i++) {
+        if (graphJSON.props[i].name.toLocaleLowerCase() === "mongocollections") {
+            return i;
+        }
+    }
+    return null;
+}
+
+function display(fContent, vertices) {
+    let service = fContent.file.split('/').filter(s => {
+        return s.toLocaleLowerCase().indexOf('service') > -1;
+    });
+    return {
+        id : service ? service[0] ? service[0] : 'sc1' : 'sc1',
+        vertices : vertices
+    }
+}
+
